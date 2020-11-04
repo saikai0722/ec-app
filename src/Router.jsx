f4a37579b1bc1e944918c8f868759fc9104d9137
西海祐太郎 <saikai.yutaro@saikai.local>
2020-11-04 23:46:08 +0900

    20201104_ecapp07 ログイン表示/

--- src/Router.jsx
+++ src/Router.jsx
@@ -5,7 +5,7 @@ import {Login, Home} from './templates';
 const Router = () => {
     return (
         <Switch>
-            <Route exact path={'./login'} component={Login} />
+            <Route exact path={'/login'} component={Login} />
             <Route exact path={'(/)?'} component={Home} />
         </Switch>
     )
