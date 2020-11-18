beb0c0946d0cef408fc12e6b94705ce499ef7276
西海祐太郎 <saikai.yutaro@saikai.local>
2020-11-18 23:31:51 +0900

    20201118_ecapp02 必要ファイルアップ

---
+++ src/firebase/config.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+export const firebaseConfig = {
+    apiKey: "AIzaSyAeGDvmh08O8MiJp32Tiec1yDD6Q9gJij4",
+    authDomain: "ec-app-bb0e3.firebaseapp.com",
+    databaseURL: "https://ec-app-bb0e3.firebaseio.com",
+    projectId: "ec-app-bb0e3",
+    storageBucket: "ec-app-bb0e3.appspot.com",
+    messagingSenderId: "1056578656006",
+    appId: "1:1056578656006:web:6cb9accc21b2600a70f087",
+    measurementId: "G-FNG6V2ERNC"
+};
