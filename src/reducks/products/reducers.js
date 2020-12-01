3e97ce2cd4aae445003ce598826ac6b83e3ad1c9
西海祐太郎 <saikai.yutaro@saikai.local>
2020-12-01 23:39:51 +0900

    20201201_menuComponent 編集・削除機能実装

--- src/reducks/products/reducers.js
+++ src/reducks/products/reducers.js
@@ -3,6 +3,11 @@ import initialState from '../store/initialState';
 
 export const ProductsReducer = (state = initialState.products, action) => { //第一引数：state　第二引数：actionがreturnした値
     switch (action.type) {
+        case Actions.DELETE_PRODUCT:
+            return {
+                ...state,
+                list: [...action.payload]
+            };
         case Actions.FETCH_PRODUCTS:
             return {
                 ...state,
